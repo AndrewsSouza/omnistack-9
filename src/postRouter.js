78e892c8daab5f47b6dad7f982b32f47ccc0120d
Andrews Souza <andrews.souza@cwi.com.br>
2019-06-13 03:02:37 -0300

    API finalizada

---
+++ src/postRouter.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const multer = require('multer')
+const uploadConfigs = require('./config/upload')
+
+const PostController = require('./controllers/PostController')
+const LikeController = require('./controllers/LikeController')
+
+const routes = express.Router()
+const upload = multer(uploadConfigs)
+
+routes.post('/posts', upload.single('image') , PostController.store)
+routes.get('/posts', PostController.index)
+
+routes.post('/posts/:id', LikeController.store)
+
+module.exports = routes
